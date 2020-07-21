088a9620520c6bd0fd1a4f33d205763afe224dd9
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-07-22 00:18:27 +0300

    Pills Added

---
+++ OrganizePill/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+   private Transform[] _pill;
+   public Transform _spawnPoint;
+
+   void Start()
+    {
+        CreatePill(); 
+    } 
+
+   private void CreatePill(){
+
+       for (int i = 0; i < _pill.Length; i++)
+       {
+            for(int j = 0; j<3; j++)
+            {
+                Instantiate(_pill[i], _spawnPoint.transform.position, Quaternion.identity);
+            } 
+           
+        }
+   }
+
+}
