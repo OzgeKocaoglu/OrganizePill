03df6b6bdc0038420ce523be2cd35fe03980863e
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-07-24 17:05:45 +0300

    Finished Bottle Classification

--- OrganizePill/Assets/Scripts/GameManager.cs
+++ OrganizePill/Assets/Scripts/GameManager.cs
@@ -3,26 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+public delegate void Pills(GameObject pill);
+
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
+
+[SerializeField]
    private Transform[] _pill;
    public Transform _spawnPoint;
 
+
    void Start()
     {
-        CreatePill(); 
+        CreatePill();
     } 
 
    private void CreatePill(){
 
        for (int i = 0; i < _pill.Length; i++)
        {
-            for(int j = 0; j<3; j++)
+            for(int j = 0; j<5; j++)
             {
-                Instantiate(_pill[i], _spawnPoint.transform.position, Quaternion.identity);
+                if (i == 0)
+                {
+                    Instantiate(_pill[i], _spawnPoint.transform.position, Quaternion.identity);
+                }
+                else if(i == 1)
+                {
+                    Instantiate(_pill[i], _spawnPoint.transform.position + new Vector3(0.1f, 0.1f, 0f), Quaternion.identity);
+                }
+                else if(i == 2)
+                {
+                    Instantiate(_pill[i], _spawnPoint.transform.position + new Vector3(-0.1f, -0.1f, 0f), Quaternion.identity);
+                }
+               
             } 
         }
    }
+
 
 }
--- OrganizePill/Assets/Scripts/InputManager.cs
+++ OrganizePill/Assets/Scripts/InputManager.cs
@@ -6,7 +6,15 @@ using UnityEngine.EventSystems;
 public class InputManager : MonoBehaviour
 {
     Vector3 distance;
+    Rigidbody _myRigidboy;
     float posX, posY;
+    float waitforsecond = 2.0f;
+    private int PL1 = 0, PL2 = 0, PL3 = 0; //pill1 bottle count, pill2 bottle count, pill3 bottle count
+
+    private void Start()
+    {
+        _myRigidboy = GetComponent<Rigidbody>();
+    }
 
     private void OnMouseDown()
     {
@@ -17,25 +25,26 @@ public class InputManager : MonoBehaviour
 
     private void OnMouseDrag()
     {
-       
+        _myRigidboy.constraints = RigidbodyConstraints.FreezeRotation;
+
         
         if(this.gameObject.tag == "pill1")
         {
-            Vector3 curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, 1.6f);
+            Vector3 curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, 1.7f);
             Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
             transform.position = worldPos;
             transform.localScale = new Vector3(2f, 2f, 2f);
         }
         else if(this.gameObject.tag == "pill2")
         {
-            Vector3 curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, 1.6f);
+            Vector3 curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, 1.7f);
             Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
             transform.position = worldPos;
             transform.localScale = new Vector3(6f, 6f, 6f);
         }
         else if(this.gameObject.tag == "pill3")
         {
-            Vector3 curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, 1.6f);
+            Vector3 curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, 1.7f);
             Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
             transform.position = worldPos;
             transform.localScale = new Vector3(10f, 10f, 10f);
@@ -47,9 +56,165 @@ public class InputManager : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "bottle1" || other.tag == "bottle2" || other.tag == "bottle3")
+        #region Dead Part
+        if (other.tag == "Dead")
         {
-            Debug.Log("Ben: " + gameObject.name + "Şu Bottle'a dokunuyorum! ::: " + other.tag);
+            Debug.Log("Dead Zone!");
+            if(gameObject.tag == "pill1"){ ReTransformPillObjects("pill1"); }
+            else if(gameObject.tag == "pill2") { ReTransformPillObjects("pill2"); }
+            else if (gameObject.tag == "pill3"){ ReTransformPillObjects("pill3"); }
+        }
+        #endregion
+
+        #region Point
+
+        if (other.tag == "bottle1")
+        {
+            if (this.gameObject.tag == "pill1")
+            {
+
+                AddPoint(1);
+                Debug.Log("You have got a point! FOR BOTTLE 1! YOUR POINT IS:: " + PL1);
+            }
+            else if(this.gameObject.tag =="pill2")
+            {
+                //Create new pill for this
+                Debug.Log("WRONG PILL:: " + this.gameObject.tag);
+                //TO DO:: ADD CROSS SIGN
+            }
+            else if(this.gameObject.tag == "pill3")
+            {
+                //Create new pill for this
+                Debug.Log("WRONG PILL:: " + this.gameObject.tag);
+                //TO DO:: ADD CROSS SIGN
+            }
+        }
+
+        if(other.tag == "bottle2")
+        {
+            if (this.gameObject.tag == "pill2")
+            {
+
+                AddPoint(2);
+                Debug.Log("You have got a point! FOR BOTTLE 2! YOUR POINT IS:: " + PL2);
+            }
+            else if (this.gameObject.tag == "pill1")
+            {
+                //Create new pill for this
+                Debug.Log("WRONG PILL:: " + this.gameObject.tag);
+                //TO DO:: ADD CROSS SIGN
+            }
+            else if (this.gameObject.tag == "pill3")
+            {
+                //Create new pill for this
+                Debug.Log("WRONG PILL:: " + this.gameObject.tag);
+                //TO DO:: ADD CROSS SIGN
+            }
+        }
+
+        if(other.tag == "bottle3")
+        {
+            if (this.gameObject.tag == "pill3")
+            {
+                AddPoint(3);
+                Debug.Log("You have got a point! FOR BOTTLE 3! YOUR POINT IS: " + PL3);
+            }
+            else if (this.gameObject.tag == "pill2")
+            {
+                //Create new pill for this
+                Debug.Log("WRONG PILL:: " + this.gameObject.tag);
+                //TO DO:: ADD CROSS SIGN
+            }
+            else if (this.gameObject.tag == "pill3")
+            {
+                //Create new pill for this
+                Debug.Log("WRONG PILL:: " + this.gameObject.tag);
+                //TO DO:: ADD CROSS SIGN
+            }
+        }
+
+        #endregion
+
+    }
+
+    IEnumerator ReSpawnObject()
+    {
+        Debug.Log("Waiting for respawn");
+        yield return new WaitForSeconds(waitforsecond);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "bottle1")
+        {
+            if(this.gameObject.tag == "pill1")
+            {
+                PL1--;
+                Debug.Log("You lose your point");
+            }
+        }
+        if(other.tag == "bottle2")
+        {
+            if(this.gameObject.tag == "pill2")
+            {
+                PL2--;
+                Debug.Log("You lose your point");
+            }
+        }
+
+        if(other.tag == "bottle3")
+        {
+            if(this.gameObject.tag == "pill3")
+            {
+                PL3--;
+                Debug.Log("You lose your point");
+            }
         }
     }
+
+
+    void ReTransformPillObjects(string pillTag)
+    {
+        if(pillTag == "pill1")
+        {
+            Debug.Log("Creating new pill 1");
+            StartCoroutine("ReSpawnObject");
+            this.gameObject.transform.position = new Vector3(-0.07f, 5.4f, 6.53f);
+            this.gameObject.transform.localScale = new Vector3(15f, 15f, 15f);
+        }
+
+        else if(pillTag == "pill2")
+        {
+            Debug.Log("Creating new pill 2");
+            StartCoroutine("ReSpawnObject");
+            this.gameObject.transform.position = new Vector3(-0.07f, 5.4f, 6.53f);
+            this.gameObject.transform.localScale = new Vector3(30f, 30f, 30f);
+        }
+
+        else if(pillTag == "pill3")
+        {
+            Debug.Log("Creating new pill 3");
+            StartCoroutine("ReSpawnObject");
+            this.gameObject.transform.position = new Vector3(-0.07f, 5.4f, 6.53f);
+            this.gameObject.transform.localScale = new Vector3(40f, 40f, 40f);
+        }
+
+    }
+
+    public void AddPoint(int pillScoreName)
+    {
+        if(pillScoreName == 1)
+        {
+            PL1++;
+        }
+        else if(pillScoreName == 2)
+        {
+            PL2++;
+        }
+        else if(pillScoreName == 3)
+        {
+            PL3++;
+        }
+    }
+
 }
